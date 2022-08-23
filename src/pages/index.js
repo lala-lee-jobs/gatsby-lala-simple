ae12401919d52534e4616b61b69464e8ed3e52e4
lala-lee-jobs <lala.lee.jobs@gmail.com>
2022-08-23 17:29:23 +0800

    Global Styling-透過Gatsby-browser.js的方式導入

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Layout from '../components/layout/layout'
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <main>
-        <title>Lala的Gatsby網站</title>
-        <h1>Welcome to Lala的Gatsby網站</h1>
-        <p>Gatsby Try Try</p>
-        <Link to="/intro/about/">About</Link>
-      </main>
-    </Layout>
+    <main>
+      <title>Lala的Gatsby網站</title>
+      <h1>Welcome to Lala的Gatsby網站</h1>
+      <p>Gatsby Try Try</p>
+      <Link to="/intro/about/">About</Link>
+    </main>
   )
 }
 
