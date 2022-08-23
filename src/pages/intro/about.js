b2584d31501d704a673a9b217a947289a7ea33df
lala-lee-jobs <lala.lee.jobs@gmail.com>
2022-08-23 16:19:35 +0800

    怎麼可能整個網站只有一頁-讓我們練習多增加一頁

---
+++ src/pages/intro/about.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>關於Lala Gatsby</title>
+      <h1>這是關於我這一頁</h1>
+    </main>
+  )
+}
+
+export default AboutPage
